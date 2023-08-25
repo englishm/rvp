f5951b2bc74a332000f9949847341ab1bdb9448a
Mike English <mike.english@gmail.com>
2023-08-25 17:41:05 -0400

    Add basic eframe/egui app

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,35 @@
-fn main() {
+use eframe::egui;
+use std::error::Error;
+
+fn main() -> Result<(), Box<dyn Error>> {
     println!("rvp: Rust Video Player");
-    println!("(not yet implemented)");
+    println!("(not yet fully implemented)");
+    let native_options = eframe::NativeOptions::default();
+    let _ = eframe::run_native(
+        "rvp: Rust Video Player",
+        native_options,
+        Box::new(|cc| Box::new(MyEguiApp::new(cc))),
+    )?;
+    Ok(())
+}
+
+#[derive(Default)]
+struct MyEguiApp {}
+
+impl MyEguiApp {
+    fn new(_cc: &eframe::CreationContext<'_>) -> Self {
+        // Customize egui here with cc.egui_ctx.set_fonts and cc.egui_ctx.set_visuals.
+        // Restore app state using cc.storage (requires the "persistence" feature).
+        // Use the cc.gl (a glow::Context) to create graphics shaders and buffers that you can use
+        // for e.g. egui::PaintCallback.
+        Self::default()
+    }
+}
+
+impl eframe::App for MyEguiApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("(not yet fully implemented)");
+        });
+    }
 }
